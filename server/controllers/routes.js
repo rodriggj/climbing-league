bb32a031b08087a8fca6226d32c2168887a80f9a
Gabe Rodriguez <gabe.rodriguez@MBP16-C02F9AE2MD6M.local>
2022-02-06 23:09:13 -0700

    added create routes logic

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Route = require('../models/Routes')
 
 //@desc     Get all Routes
 //@route    GET /api/v1/routes
@@ -21,9 +22,55 @@ exports.getRoutesById = async (req, res, next) => {
 
 exports.createRoute = async (req, res, next) => {
     try{
-        res.send('Creates a new Route')
+        let existingRoute = await Route.findOne({ week: req.body.week, route_number: req.body.route_number })
+
+        if(existingRoute){
+            return res.status(400).json({
+                success: "Failed", 
+                msg: "Route number for this week already exists."
+            })
+        }
+
+        const { 
+            route_rating, 
+            week, 
+            route_number, 
+            route_point_value, 
+            setter, route_stars, 
+            route_attempts, 
+            route_flashed_qy, 
+            route_2nd_attempt_qty, 
+            route_3rd_attempt_qty,
+            route_4th_attempt_qty,
+            route_5th_attempt_qty
+         } = req.body
+
+         let newRoute = new Route( { 
+            route_rating, 
+            week, 
+            route_number, 
+            route_point_value, 
+            setter, route_stars, 
+            route_attempts, 
+            route_flashed_qy, 
+            route_2nd_attempt_qty, 
+            route_3rd_attempt_qty,
+            route_4th_attempt_qty,
+            route_5th_attempt_qty
+         } )
+
+         await newRoute.save()
+
+         res.status(201).json({
+             status: "Success", 
+             msg: "New Route Created", 
+             data: newRoute
+         })
     } catch(err){
-        res.send(err)
+        res.status(400).json({ 
+            status: "Failed", 
+            msg: err.message
+        })
     }
 }
 
